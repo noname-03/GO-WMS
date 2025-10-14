9975a289a154d61ce57afadcca4ba7f07047b35b
Fahrurrozi <Fahrurrozi.arul3@gmail.com>
2025-10-14 18:02:36 +0700

    fix: Add missing route for fetching product batches by product ID

--- internal/routes/v1/productbatch/product_batch_routes.go
+++ internal/routes/v1/productbatch/product_batch_routes.go
@@ -14,11 +14,12 @@ func RegisterProductBatchRoutes(app fiber.Router) {
 	productBatchRoutes.Use(middleware.JWTMiddleware())
 
 	// CRUD operations for product batches
-	productBatchRoutes.Get("/", handler.GetProductBatches)               // GET /api/v1/product-batches
-	productBatchRoutes.Get("/deleted", handler.GetDeletedProductBatches) // GET /api/v1/product-batches/deleted
-	productBatchRoutes.Get("/:id", handler.GetProductBatchByID)          // GET /api/v1/product-batches/:id
-	productBatchRoutes.Post("/", handler.CreateProductBatch)             // POST /api/v1/product-batches
-	productBatchRoutes.Put("/:id", handler.UpdateProductBatch)           // PUT /api/v1/product-batches/:id
-	productBatchRoutes.Put("/:id/restore", handler.RestoreProductBatch)  // PUT /api/v1/product-batches/:id/restore
-	productBatchRoutes.Delete("/:id", handler.DeleteProductBatch)        // DELETE /api/v1/product-batches/:id
+	productBatchRoutes.Get("/", handler.GetProductBatches)                            // GET /api/v1/product-batches
+	productBatchRoutes.Get("/deleted", handler.GetDeletedProductBatches)              // GET /api/v1/product-batches/deleted
+	productBatchRoutes.Get("/product/:productId", handler.GetProductBatchesByProduct) // GET /api/v1/product-batches/product/:productId
+	productBatchRoutes.Get("/:id", handler.GetProductBatchByID)                       // GET /api/v1/product-batches/:id
+	productBatchRoutes.Post("/", handler.CreateProductBatch)                          // POST /api/v1/product-batches
+	productBatchRoutes.Put("/:id", handler.UpdateProductBatch)                        // PUT /api/v1/product-batches/:id
+	productBatchRoutes.Put("/:id/restore", handler.RestoreProductBatch)               // PUT /api/v1/product-batches/:id/restore
+	productBatchRoutes.Delete("/:id", handler.DeleteProductBatch)                     // DELETE /api/v1/product-batches/:id
 }
